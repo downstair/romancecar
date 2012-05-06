ca13d277f83ee56327684f2779493d957c097197
yuya hidaka <user@yuya-hidaka-no-MacBook-Pro.local>
2012-05-06 13:29:42 +0900

    modify: gems

--- config/initializer.rb
+++ config/initializer.rb
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 require 'mongoid'
-require 'sinatra/reloader' if development?
+# require 'sinatra/reloader' if development?
 require 'open-uri'
 require 'haml'
 require 'sass'
 require 'yaml'
-require 'httpclient'
+# require 'httpclient'
 require 'json/pure'
 require 'uri'
 require 'rack/csrf'
